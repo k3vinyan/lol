a34d0f2e837d250507297774fe4cde234e2b8582
Kevin Yan <k3vinyan@gmail.com>
2019-08-06 16:38:44 -0700

    update playerapi to use fetch instead of xhr

--- api.js
+++ api.js
@@ -7,6 +7,10 @@ function getChampionsApi()  {
         .then(function(response) {
             return response.json()
         })
+        .then(function(response) {
+            return response.data;
+        })
+        
         .catch(function(error) {
             console.log("Error in Champion API: ", error)
             return;
@@ -15,25 +19,34 @@ function getChampionsApi()  {
 
 function getPlayersApi() {
     const url = CONSTANTS.PLAYERJSON;
-    const promise = new Promise( function(resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-            const status = xhr.status;
 
-            if(status === 200) {
-                console.log("get champions success!")
-                resolve(xhr.response)
-            } else {
-                reject("Error: " + status)
-            }
-        }
+    return fetch(url)
+        .then(function(response) {
+            return response.json()
+        })
+        .catch(function(error) {
+            console.log("Error in Player API: ", error)
+        })
+    // const url = CONSTANTS.PLAYERJSON;
+    // const promise = new Promise( function(resolve, reject) {
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.open('GET', url, true);
+    //     xhr.responseType = 'json';
+    //     xhr.onload = function() {
+    //         const status = xhr.status;
 
-        xhr.send()
-    })
+    //         if(status === 200) {
+    //             console.log("get champions success!")
+    //             resolve(xhr.response)
+    //         } else {
+    //             reject("Error: " + status)
+    //         }
+    //     }
 
-    return promise;
+    //     xhr.send()
+    // })
+
+    // return promise;
 }
 
 export { getChampionsApi, getPlayersApi };
