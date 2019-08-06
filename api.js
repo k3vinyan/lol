e3e79154ab97a4b16f1d71c1e80c9018e1e10fc0
Kevin Yan <k3vinyan@gmail.com>
2019-08-06 15:22:59 -0700

    change getChampionapi function to use fetch instead of xhr

---
+++ api.js
@@ -0,0 +1,39 @@
+import { CONSTANTS } from './constants.js';
+
+function getChampionsApi()  {
+    const url = CONSTANTS.CHAMPIONJSON;
+
+    return fetch(url)
+        .then(function(response) {
+            return response.json()
+        })
+        .catch(function(error) {
+            console.log("Error in Champion API: ", error)
+            return;
+        })
+}
+
+function getPlayersApi() {
+    const url = CONSTANTS.PLAYERJSON;
+    const promise = new Promise( function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            const status = xhr.status;
+
+            if(status === 200) {
+                console.log("get champions success!")
+                resolve(xhr.response)
+            } else {
+                reject("Error: " + status)
+            }
+        }
+
+        xhr.send()
+    })
+
+    return promise;
+}
+
+export { getChampionsApi, getPlayersApi };
