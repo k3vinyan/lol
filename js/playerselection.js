4f251245f414d1440156285ad31c92897da0b25b
Kevin Yan <k3vinyan@gmail.com>
2019-08-08 16:44:21 -0700

    move slide to bottom of page

--- js/playerselection.js
+++ js/playerselection.js
@@ -25,65 +25,20 @@ mid.addEventListener('click', (el)=> {
             const imgurl = CONSTANTS.URL.IMG + champ.image.full;
             player.firstChild.src = imgurl;
         })
-    }
-
-    //lockin button
-    if(elClassName === "button") {
+    }else if(elClassName === "button") {
         let champ = localStorage.getItem('selected');
 
         allskinsCount.then(function(response) {
             const skinCount = parseInt(response[champ]) + 1;
             mid.innerHTML = "";
-
-
             const skinWrapper = new SkinsCarousel('div', 'carousel');
             skinWrapper.addSkins(champ, skinCount);
             mid.appendChild(skinWrapper.el)
 
         })
+    } else {
+        toggelDisplay(elClassName);
     }
-    let className = el.target.className;
-
-    toggelDisplay(elClassName);
-
-    // //next button
-    // if(elClassName === "next") {
-    //     let skins = document.getElementsByClassName("skin-wrapper");
-
-    //     for(let i = 0; i < skins.length; i++) {
-    //         if(skins[skins.length-1].classList.contains("show")) {
-    //             break;
-    //         }
-    //         if(skins[i].classList.contains("show")) {
-    //             skins[i].classList.remove("show");
-    //             skins[i].classList.add("hide");
-
-    //             skins[i+5].classList.remove("hide");
-    //             skins[i+5].classList.add("show");
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // //prev button
-    // if(elClassName === "prev") {
-    //     let skins = document.getElementsByClassName("skin-wrapper");
-    //     console.log("prev")
-    //     for(let i = 0; i < skins.length; i++) {
-    //         if(skins[0].classList.contains("show")) {
-    //             break;
-    //         }
-
-    //         if(skins[i].classList.contains("show")) {
-    //             console.log("loop")
-    //             skins[i-1].classList.remove("hide");
-    //             skins[i-1].classList.add("show");
-
-    //             skins[i+4].classList.remove("show");
-    //             skins[i+4].classList.add("hide");
-    //         }
-    //     }
-    // }
 })
 
 function toggelDisplay(className){
@@ -118,7 +73,6 @@ function toggelDisplay(className){
                 skins[i-1].classList.remove("hide");
                 skins[i-1].classList.add("show");
 
-                console.log("hello")
                 skins[i+4].classList.remove("show");
                 skins[i+4].classList.add("hide");
                 break;
